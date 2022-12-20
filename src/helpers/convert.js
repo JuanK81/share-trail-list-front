786fd0810b46c75f4738c944a93bf1c3ad943058
JuanK81 <82944967+JuanK81@users.noreply.github.com>
2022-12-20 11:44:03 +0100

    Update convert.js

--- src/helpers/convert.js
+++ src/helpers/convert.js
@@ -4,21 +4,8 @@ const convert = (file) => {
   const parser = new DOMParser();
 
   const parsed = parser.parseFromString(file, 'text/xml');
-  // console.log('PARSED__', parsed);
   const geoJson = gpx(parsed);
-  // console.log('GEO_JSON__', geoJson.features[0]);
-  // console.log('GEO_JSON__type', geoJson.features[0].properties.type);
   return geoJson;
 };
-// const convert = async (file) => {
-//   const parser = new DOMParser();
-
-//     const parsed = await parser.parseFromString(file, 'text/xml');
-//     console.log('PARSED__', parsed);
-//     const geoJson = await gpx(parsed);
-//     console.log('GEO_JSON__', geoJson);
-//     return geoJson;
-
-// };
 
 export default convert;
